418498dbdbe778c1bdc4a6a12ec316a4b1410a3e
Ward9250 <ward9250@gmail.com>
2016-11-02 21:45:51 +0000

    Setup automated documentation site building.

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,9 @@
 using Documenter, WAHVectors
 
-custom_deps() = run(`pip install --user pygments mkdocs mkdocs-biojulia`)
 makedocs()
 deploydocs(
-           repo = "github.com/BioJulia/WAHVectors.jl.git",
-           julia = "0.5",
-           osname = "linux",
-           deps = custom_deps
-           )
+    deps = Deps.pip("mkdocs", "pygments", "mkdocs-biojulia"),
+    repo = "github.com/BioJulia/WAHVectors.jl.git",
+    julia = "0.5",
+    osname = "linux"
+)
