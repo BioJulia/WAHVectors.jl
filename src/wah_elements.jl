e4eba34eaf52583ac037da264f18e24386150c88
Ward9250 <ward9250@gmail.com>
2016-11-15 23:07:39 +0000

    Edited show method for WAHElements

--- src/wah_elements.jl
+++ src/wah_elements.jl
@@ -136,9 +136,10 @@ end
 nbits(x::WAHElement) = UInt64(31) * UInt64(nwords(x))
 
 function Base.show(io::IO, element::WAHElement)
+    rf = hex(runfill(element))
     if isruns(element)
-        println(io, "$(nruns(element)) compressed $(runfill(element)) words.")
+        println(io, "$(nruns(element)) compressed $rf words.")
     else
-        println(io, "1 literal $(runfill(element)).")
+        println(io, "1 literal $rf.")
     end
 end
