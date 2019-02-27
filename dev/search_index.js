03a2ad39426b731d452d78590768ee102d328c96
autodocs <autodocs>
2019-02-27 15:15:09 +0000

    build based on 4c9108f

---
+++ dev/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#WAHVectors.jl-Documentation-1",
+    "page": "Home",
+    "title": "WAHVectors.jl Documentation",
+    "category": "section",
+    "text": "Test"
+},
+
+{
+    "location": "man/api_elements/#",
+    "page": "WAHElements",
+    "title": "WAHElements",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "man/api_elements/#API:-WAHElements-1",
+    "page": "WAHElements",
+    "title": "API: WAHElements",
+    "category": "section",
+    "text": "isruns(x::WAHElement)\nisliteral(x::WAHElement)\nis_zeros_runs(x::WAHElement)\nis_ones_runs(x::WAHElement)\nnwords(x::WAHElement)\nrunval(x::WAHElement)\nnruns(x::WAHElement)\nnfree(x::WAHElement)\nisfull(x::WAHElement)"
+},
+
+]}
